ccca44d0b37773912885f12d9e70bec7ddc36192
M-Schwab <matthewschwab256@gmail.com>
2025-03-15 17:21:31 -0700

    inital commit

---
+++ src/app.rs
@@ -0,0 +1,152 @@
+use leptos::task::spawn_local;
+use leptos::{ev::SubmitEvent, prelude::*};
+use gloo_net::http::Request;
+use thaw::{ColorPicker, ConfigProvider};
+use thaw::Color;
+use palette::Srgb;
+
+use crate::teamgen::{Player, get_even_teams};
+
+fn get_color_code(c: Color) -> String {
+    let c: Srgb<u8> = match c {
+        Color::RGB(c) => c,
+        _ => panic!("AHHH"),
+    }.into_format();
+    format!("rgb({}, {}, {})", 
+        c.red, 
+        c.green, 
+        c.blue
+    )
+}
+
+#[component]
+pub fn App() -> impl IntoView {
+
+    let (player_sheet_url, set_player_sheet_url) = signal(String::new());
+    let (players_sig, set_players) = signal(vec![]);
+
+    // Team signals
+    let (get_team_a, set_team_a) = signal(vec![]);
+    let (get_team_b, set_team_b) = signal(vec![]);
+
+    let team_a_color = RwSignal::new(Color::from(Srgb::new(255.0, 123.0, 0.0)));
+    let team_b_color = RwSignal::new(Color::from(Srgb::new(0.0, 123.0, 255.0)));
+
+    let update_player_sheet_url = move |ev| {
+        let v = event_target_value(&ev);
+        set_player_sheet_url.set(v);
+    };
+
+    let players_event = move |ev: SubmitEvent| {
+        ev.prevent_default();
+        spawn_local(async move {
+            let mut player_sheet_url = player_sheet_url.get_untracked();
+            if player_sheet_url.is_empty() {
+                player_sheet_url.push_str("https://docs.google.com/spreadsheets/d/14dwWyphNZViiwSOMsvWCGZYuPfuRSqy1FbaVhwlZpj0/edit?gid=0#gid=0");
+            }
+
+            let csv_export_url = "https://docs.google.com/spreadsheets/d/14dwWyphNZViiwSOMsvWCGZYuPfuRSqy1FbaVhwlZpj0/export?format=csv&gid=0";
+
+            let csv_resp = Request::get(csv_export_url)
+                .send()
+                .await.unwrap();
+
+            let players_csv = csv_resp.text().await.unwrap();
+
+            let mut lines = players_csv.split("\n");
+
+            while let Some(l) = lines.next() {
+                if l.starts_with("fName") {
+                    break;
+                }
+            }
+            set_players.set(lines.filter_map(|l| Player::from_row(l)).collect());
+        });
+    };
+
+    let team_gen_event = move |ev: SubmitEvent| {
+        //TODO: implement me!
+        ev.prevent_default();
+        spawn_local( async move {
+            let players = players_sig.get();
+            let (a, b) = get_even_teams(&players, 1.0);
+
+            set_team_a.set(a);
+            set_team_b.set(b);
+        });
+    };
+
+    view! {
+        <main class="container">
+            <h1>"Team Builder"</h1>
+            <img src="public/tauri.svg" class="logo tauri" alt="Tauri logo"/>
+
+            <form class="row" on:submit=players_event>
+                <input
+                    id="team-sheet-input"
+                    placeholder="Enter team google sheet url..."
+                    on:input=update_player_sheet_url
+                />
+                <button type="submit">"Refresh player list"</button>
+            </form>
+
+            <div class="row">
+                <div>
+                    <h3>" Participating Players: ("{ move || players_sig.get().len() }")"</h3>
+                    <table id="player-listing">
+                        <tr>
+                            <th> Name </th><th> Rating </th><th> Gender </th><th> Team Lock </th>
+                        </tr>
+                        { move || players_sig.get().into_iter()
+                            .map(|p| view!{ <tr>
+                                <td>{p.name}</td>
+                                <td>{p.rating}</td>
+                                <td>{ move || if p.gender {"F"} else {"M"} }</td>
+                                <td>{ move || {
+                                    if let Some(fixed) = p.fixed_team {
+                                        if fixed {
+                                            "A"
+                                        } else {
+                                            "B"
+                                        }
+                                    } else {
+                                        ""
+                                    }
+                                } }</td>
+                            </tr> })
+                            .collect_view() }
+                    </table>
+                </div>
+                <div id="teams">
+                    <ConfigProvider>
+                        <ColorPicker value=team_a_color/>
+                        <ColorPicker value=team_b_color/>
+                    </ConfigProvider>
+                    <form class="row" on:submit=team_gen_event>
+                        <button type="submit">Generate Teams</button>
+                    </form>
+                    <div class="row">
+                    <div>
+                    <table id="generated-teams">
+                        <tr>
+                            <th> Team A </th><th> Team B </th>
+                        </tr>
+                        { move || get_team_a.get().into_iter()
+                            .zip(get_team_b.get())
+                            .map(|(a, b)| view!{ <tr>
+                                <td style:background-color=move || get_color_code(team_a_color.get())>{a.name}</td>
+                                <td style:background-color=move || get_color_code(team_b_color.get())>{b.name}</td>
+                            </tr> })
+                            .collect_view() }
+                        <tr>
+                            <td>"Score: "{ move || get_team_a.get().iter().map(|p| p.rating - 5.0).sum::<f32>()}</td>
+                            <td>"Score: "{ move || get_team_b.get().iter().map(|p| p.rating - 5.0).sum::<f32>()}</td>
+                        </tr>
+                    </table>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    }
+}
---
+++ src/teamgen.rs
@@ -0,0 +1,92 @@
+use serde::{Serialize, Deserialize};
+use rand::thread_rng;
+use rand::seq::SliceRandom;
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Player {
+    pub name: String,
+    pub rating: f32,
+    pub gender: bool,
+    pub fixed_team: Option<bool>,
+}
+
+impl Player {
+    pub fn from_row(value: &'_ str) -> Option<Self> {
+        let mut player_fields = value.split(",");
+
+        let mut name = player_fields.next().unwrap().to_string();
+        name.push(' ');
+        name.push_str(player_fields.next().unwrap());
+        let gender = player_fields.next().unwrap() == "F";
+        let rating = player_fields.next().unwrap().parse().unwrap();
+
+        let attending = player_fields.next().unwrap() == "y";
+
+        if attending {
+            Some(Player {
+                name,
+                rating,
+                gender,
+                fixed_team: None
+            })
+        } else {
+            None
+        }
+    }
+}
+
+pub fn get_even_teams(players: &[Player], max_delta: f32)  -> (Vec<Player>, Vec<Player>) {
+    for _ in 0..100_000 {
+        let (a, b) = random_team(players);
+        let a_rating: f32 = a.iter().map(|p| p.rating).sum();
+        let b_rating: f32 = b.iter().map(|p| p.rating).sum();
+        if (a_rating - b_rating).abs() < max_delta {
+            return (a, b)
+        }
+    }
+    panic!("Couldn't create a")
+}
+
+
+pub fn random_team(players: &[Player]) -> (Vec<Player>, Vec<Player>) {
+
+    let player_count = players.len();
+
+    let mut team_a = players.iter()
+        .filter(|p| p.fixed_team.is_some_and(|f| f))
+        .map(|p| p.clone())
+        .collect::<Vec<_>>();
+
+    let mut team_b = players.iter()
+        .filter(|p| p.fixed_team.is_some_and(|f| !f))
+        .map(|p| p.clone())
+        .collect::<Vec<_>>();
+
+    let mut players = players.iter()
+        .filter(|p| p.fixed_team.is_none())
+        .map(|p| p.clone())
+        .collect::<Vec<_>>();
+
+    players.shuffle(&mut thread_rng());
+    
+    while team_a.len() < player_count / 2 {
+        team_a.push(players.pop().unwrap())
+    }
+
+    while team_b.len() < player_count / 2 {
+        team_b.push(players.pop().unwrap())
+    }
+
+    if players.len() == 1 {
+        let mut shared_player = players.pop().unwrap();
+        let player_name = shared_player.name.clone();
+
+        let mut copy =  shared_player.clone();
+        copy.name = format!("{player_name} (1st half)");
+        shared_player.name = format!("{player_name} (2nd half)");
+        team_a.push(copy);
+        team_b.push(shared_player);
+    }
+
+    (team_a, team_b)
+}
