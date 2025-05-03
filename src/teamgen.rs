26b43a59ffa5fca2e526a3e8e6e0999ec6e77c96
M-Schwab <matthewschwab256@gmail.com>
2025-05-03 09:46:15 -0700

    use references when generating teams to make it faster

--- src/teamgen.rs
+++ src/teamgen.rs
@@ -14,6 +14,41 @@ pub struct Player {
     pub position: Option<Vec<String>>,
 }
 
+pub struct Team<'a> {
+    pub players: Vec<&'a Player>,
+    pub half_player: Option<&'a Player>,
+}
+
+impl<'a> Team<'a> {
+    pub fn owned(&'a self, half: usize) -> Vec<Player> {
+        let mut players: Vec<Player> = self.players.iter()
+            .map(|&p| p.clone())
+            .collect();
+
+        match self.half_player {
+            Some(p) => {
+                let name = if half == 1 {
+                    format!("{} (1st half)", p.name)
+                } else if half == 2 {
+                    format!("{} (2nd half)", p.name)
+                } else {
+                    panic!("???? got half {half}");
+                };
+                let p = Player {
+                    name,
+                    fixed_team: p.fixed_team,
+                    gender: p.gender,
+                    rating: p.rating,
+                    position: p.position.clone(),
+                };
+                players.push(p);
+            },
+            None => {},
+        };
+        players
+    }
+}
+
 impl Player {
     pub fn from_row(value: &'_ str) -> Option<Self> {
         let mut player_fields = value.split(",");
@@ -68,37 +103,30 @@ impl Player {
     }
 }
 
-fn rating(team: &Vec<Player>) -> f32 {
-    team.iter().map(|p| p.rating).sum()
+fn rating(team: &Team<'_>) -> f32 {
+    let rating: f32 = team.players.iter().map(|p| p.rating).sum();
+    rating + team.half_player.map(|p| p.rating).unwrap_or(0.0)
 }
 
-fn min_pos_met(team: &Vec<Player>, min_positions: &BTreeMap<String, usize>) -> bool {
+fn min_pos_met(team: &Vec<&Player>, min_positions: &BTreeMap<String, usize>) -> bool {
     min_positions.iter()
         .all(|(pos, req)| {
-            let gk_ok = if pos == "gk" {
-                !team.iter().any(|p| match &p.position {
-                    Some(v) => v.iter().any(|p_pos| p_pos == pos) && p.name.ends_with("(1st half)"),
-                    None => false
-                })
-            } else {
-                true
-            };
             let pos_count = team.iter().filter(|p| match &p.position {
                 Some(v) => v.iter().any(|p_pos| p_pos == pos),
                 None => false,
             })
             .count();
-
-            pos_count >= *req && gk_ok
+            pos_count >= *req
         })
 }
 
 pub fn get_even_teams(players: &[Player], max_delta: f32, min_positions: &BTreeMap<String, usize>)  -> Result<(Vec<Player>, Vec<Player>), String> {
-    for _ in 0..50_000 {
+    for _ in 0..100_000 {
         let (a, b) = random_team(players);
         if (rating(&a) - rating(&b)).abs() < max_delta {
-            if min_pos_met(&a, min_positions) && min_pos_met(&b, min_positions) {
-                return Ok((a, b))
+            // The shared player doesn't count for meeting the minimum position counts.
+            if min_pos_met(&a.players, min_positions) && min_pos_met(&b.players, min_positions) {
+                return Ok((a.owned(1), b.owned(2)))
             }
         }
     }
@@ -106,23 +134,20 @@ pub fn get_even_teams(players: &[Player], max_delta: f32, min_positions: &BTreeM
 }
 
 
-pub fn random_team(players: &[Player]) -> (Vec<Player>, Vec<Player>) {
+pub fn random_team<'a>(players: &'a [Player]) -> (Team<'a>, Team<'a>) {
 
     let player_count = players.len();
 
     let mut team_a = players.iter()
         .filter(|p| p.fixed_team.is_some_and(|f| f))
-        .map(|p| p.clone())
         .collect::<Vec<_>>();
 
     let mut team_b = players.iter()
         .filter(|p| p.fixed_team.is_some_and(|f| !f))
-        .map(|p| p.clone())
         .collect::<Vec<_>>();
 
     let mut players = players.iter()
         .filter(|p| p.fixed_team.is_none())
-        .map(|p| p.clone())
         .collect::<Vec<_>>();
 
     players.shuffle(&mut thread_rng());
@@ -135,16 +160,10 @@ pub fn random_team(players: &[Player]) -> (Vec<Player>, Vec<Player>) {
         team_b.push(players.pop().unwrap())
     }
 
-    if players.len() == 1 {
-        let mut shared_player = players.pop().unwrap();
-        let player_name = shared_player.name.clone();
-
-        let mut copy =  shared_player.clone();
-        copy.name = format!("{player_name} (1st half)");
-        shared_player.name = format!("{player_name} (2nd half)");
-        team_a.push(copy);
-        team_b.push(shared_player);
-    }
-
-    (team_a, team_b)
+    
+    let half_player = players.pop();
+    (
+        Team { players: team_a, half_player },
+        Team { players: team_b, half_player }
+    )
 }
